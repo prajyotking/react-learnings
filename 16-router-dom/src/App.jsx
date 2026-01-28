6fe2659de2a43c475c80c10e8e8016c7565f1dba
prajyotking <prajyotghosalkar2004@gmail.com>
2026-01-29 00:57:49 +0530

    huge progress

--- 16-router-dom/src/App.jsx
+++ 16-router-dom/src/App.jsx
@@ -3,12 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 
-const App = () => {
+const App = () => { 
   return (
     <div>
-      App
+     <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/contact' element={<Contact />}/>
